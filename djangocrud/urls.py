32b67f6154e20ee61d4bbdce5de735d2d078eb51
MiltonAF <mafelizzolam@gmail.com>
2023-09-28 07:38:09 -0500

    guardando los datos del registro

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('signup/', views.signup)
+    path('',views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
