19b71be96099691c4f70a9b7d9b77adb24d6a994
MiltonAF <mafelizzolam@gmail.com>
2023-09-30 17:02:41 -0500

    creando el registro de tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -32,3 +33,37 @@ def signup(request):
 
 def tasks(request):
     return render(request, "tasks.html")
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, 'create_task.html', {"form": TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm})
+
+    else:
+
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, "signin.html", {'form': AuthenticationForm, 'error': 'username of password incorret'})
+        else:
+            login(request,user)
+            return redirect('tasks')
+
+       
