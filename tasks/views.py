f12c17be9ebfcb13670c07276a4a80f3996f0ae9
MiltonAF <mafelizzolam@gmail.com>
2023-10-15 08:39:54 -0500

    actualizando

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -30,9 +33,14 @@ def signup(request):
 
         return render(request, 'signup.html', {"form": UserCreationForm, "error": 'Password do not match'})
 
-
+@login_required
 def tasks(request):
-    return render(request, "tasks.html")
+    tasks = Task.objects.filter(user=request.user, datecomplete__isnull=True)
+    return render(request, 'tasks.html', {"tasks": tasks})
+
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecomplete__isnull=False).order_by('-datecomplete')
+    return render(request, 'tasks.html', {"tasks": tasks})
 
 def create_task(request):
     if request.method == "GET":
@@ -46,6 +54,33 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
+
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecomplete = timezone.now()
+        task.save()
+        return redirect('tasks')
+    
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 def signout(request):
     logout(request)
